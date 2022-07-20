21ba415bd2e707c6285614dcf0ce03b5fd8f3c26
lethinhi2002 <kevaeirlys@gmail.com>
2022-07-20 10:40:14 +0700

    Write a program that enters three numbers, checks if they are three valid sides of a triangle

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,19 @@
-import java.util.Scanner;
-public class Main {
+
+
+   import java.util.Scanner;
+    public class Main {
+
         public static void main(String args[]) {
-            int x, y,tong,hieu,nhan,chia,phan_tram;
+            int x, y, z;
             Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter two numbers : ");
+            System.out.print("Enter the 3 sides of the triangle: ");
             x = scanner.nextInt();
             y = scanner.nextInt();
-            tong = x + y;
-            hieu = x - y;
-            nhan = x * y;
-            chia = x / y;
-            phan_tram = x%y;
-            System.out.println("Tong = " +tong);
-            System.out.println("Hieu = "+hieu);
-            System.out.println("Nhan = "+nhan);
-            System.out.println("Chia = "+chia);
-            System.out.println("Phan tram = "+phan_tram);
-        }
-    }
+            z = scanner.nextInt();
 
+            if ((x + y > z) && (y + z > x) && (z + x > y)){
+            System.out.print("valid");}
+            else {System.out.print("invalid");}
+return;
+        }
+}
