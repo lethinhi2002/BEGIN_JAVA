f70355f0bcafff0db94fd10c0884291237d2a841
lethinhi2002 <kevaeirlys@gmail.com>
2022-07-20 20:00:26 +0700

    Write a program to Rank student based on average score

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,39 @@
 
 
-   import java.util.Scanner;
-    public class Main {
+import java.util.Scanner;
+public class Main {
 
-        public static void main(String args[]) {
-            int x, y, z;
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter the 3 sides of the triangle: ");
-            x = scanner.nextInt();
-            y = scanner.nextInt();
-            z = scanner.nextInt();
-
-            if ((x + y > z) && (y + z > x) && (z + x > y)){
-            System.out.print("valid");}
-            else {System.out.print("invalid");}
-return;
+    public static void main(String args[]) {
+        int n, i;
+        int marks[] = new int[12];
+        float total = 0, avg;
+        //n là số môn học cần tính
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of subjects to be counted: ");
+        n = scanner.nextInt();
+        for (i = 0; i < n; i++) {
+            System.out.print("Enter the grade of the subject" + (i + 1) + ":");
+            marks[i] = scanner.nextInt();
+            total = total + marks[i];
         }
-}
+        avg = total / n;
+        System.out.println("AVG " + n + " subject: " + avg);
+        System.out.print(" Rank student: ");
+        if (avg < 5) {
+            System.out.print("bad");
+        } else if (avg >= 5 && avg < 7) {
+            System.out.print("Average");
+        } else if (avg >= 7 && avg < 8) {
+            System.out.print("good");
+        } else if (avg >= 8 && avg < 9) {
+            System.out.print("very good");
+        }
+           else  if (avg>=9){ System.out.print("excellent ");
+           }
+
+    }}
+
+
+
+
+
