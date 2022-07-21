4dcaa39b7eff55480d9d7ca7fe844dca23d4de39
lethinhi2002 <kevaeirlys@gmail.com>
2022-07-21 20:00:17 +0700

    Write a piece of code that reads a shorthand text description of a color

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String args[]) {
-        int n, i;
-        int marks[] = new int[12];
-        float total = 0, avg;
-        //n là số môn học cần tính
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number of subjects to be counted: ");
-        n = scanner.nextInt();
-        for (i = 0; i < n; i++) {
-            System.out.print("Enter the grade of the subject" + (i + 1) + ":");
-            marks[i] = scanner.nextInt();
-            total = total + marks[i];
+        System.out.print("What color do you want? ");
+        String color = scanner.next();
+        if(color.equalsIgnoreCase("R")) {
+            System.out.println("You have chosen Red.");
+        } else if(color.equalsIgnoreCase("B")) {
+            System.out.println("You have chosen Blue.");
+        } else if(color.equalsIgnoreCase("G")) {
+            System.out.println("You have chosen Green.");
+        } else {
+            System.out.println("Unknown color: " + color);
         }
-        avg = total / n;
-        System.out.println("AVG " + n + " subject: " + avg);
-        System.out.print(" Rank student: ");
-        if (avg < 5) {
-            System.out.print("bad");
-        } else if (avg >= 5 && avg < 7) {
-            System.out.print("Average");
-        } else if (avg >= 7 && avg < 8) {
-            System.out.print("good");
-        } else if (avg >= 8 && avg < 9) {
-            System.out.print("very good");
-        }
-           else  if (avg>=9){ System.out.print("excellent ");
-           }
 
-    }}
+        }
+    }
 
 
 
