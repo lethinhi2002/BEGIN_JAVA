52baf7ff0b285f475b33c5331419ae946c4df2ce
lethinhi2002 <kevaeirlys@gmail.com>
2022-07-26 11:09:58 +0700

    Write a method called median that accepts an array of integers as its //argument and returns the median of the numbers in the array. The median is //the number that will appear in the middle if you arrange the elements in order. //Assume that the array is of odd size (so that one sole element constitutes the

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,24 @@
-//. Write a program that prompts the user for many integers and print the total
-//even sum and maximum of the even numbers. You may assume that the user
-//types at least one non-negative even integer.
-//how many integers? 4
-//next integer? 2
-//next integer? 9
-//next integer? 18
-//next integer? 4
-//even sum = 24
-//even max = 18
+//.Write a method called median that accepts an array of integers as its
+//argument and returns the median of the numbers in the array. The median is
+//the number that will appear in the middle if you arrange the elements in order.
+//Assume that the array is of odd size (so that one sole element constitutes the
+//median) and that the numbers in the array are between 0 and 99 inclusive.
 
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] a = new int[1000];
-        System.out.print("how many integers? ");
-        int n = input.nextInt();
-        int max = 0, total = 0;
-        int m = 1;
-        while (m <= n) {
-            System.out.print("next integer? ");
-            a[m] = input.nextInt();
-            if (a[m] % 2 == 0) {
-                total += a[m];
-                if (a[m] > max) max = a[m];
-            }
-            m++;
+        System.out.print(" number of elements in the array= ");
+        int n=input.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i <n ; i++) {
+            System.out.print("a["+(i)+"]= ");
+            a[i]=input.nextInt();
         }
-        System.out.println("even sum = "+total);
-        System.out.println("even max = "+max);
+        Arrays.sort(a);
+        System.out.println("median of the numbers in the array= "+a[(a.length)/2]);
+
     }
 }
+
