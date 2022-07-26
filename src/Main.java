dbcd1fb3edcc71ce6fde25c1694297ae83a1907b
lethinhi2002 <kevaeirlys@gmail.com>
2022-07-26 10:55:25 +0700

    Write a method named lastIndexOf that accepts an array of integers and //an integer value as its parameters and returns the last index at which the //value occurs in the array.

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,52 @@
 
 
-import java.util.Scanner;
-public class Main {
+//Write a program to read an integer number (n>0). Write flowing methods:
+//- Checking whether the input number is a prime number or not?
+//- Print out prime submultiples of number n
+//- Generate the first 100 prime numbers.
+    import java.util.*;
+    public class Main {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            int[] a = new int[1000];
+            System.out.print("n: ");
+            int n = input.nextInt();
+            System.out.println(n+(isPrime(n)?" is":" is not")+" a prime number");
+            System.out.print("Prime submultiples of "+n+": ");
+            for (int i = 1; i <=n ; i++) {
+                if (n%i==0) if (isPrime(i)) System.out.print(i+" ");
+            }
+            System.out.println();
+            for (int i=1;i<=100;i++) {
+                if (isPrime(i)) System.out.print(i + " ");
+            }
 
-    public static void main(String args[]) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("What color do you want? ");
-        String color = scanner.next();
-        if(color.equalsIgnoreCase("R")) {
-            System.out.println("You have chosen Red.");
-        } else if(color.equalsIgnoreCase("B")) {
-            System.out.println("You have chosen Blue.");
-        } else if(color.equalsIgnoreCase("G")) {
-            System.out.println("You have chosen Green.");
-        } else {
-            System.out.println("Unknown color: " + color);
         }
-
+        public static boolean isPrime(int n) {
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0)
+                    return false;
+            }
+            if (n==1) return false;
+            return true;
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
